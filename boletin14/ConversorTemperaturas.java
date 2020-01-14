6a7c1577a8a1ae509dacd7405ac6d39cea5ccc95
Xavier De Anta <xdeantabelisario@danielcastelao.org>
2020-01-14 10:09:06 +0100

    Modificacion Excepcion

--- boletin14/ConversorTemperaturas.java
+++ boletin14/ConversorTemperaturas.java
@@ -19,16 +19,16 @@ public class ConversorTemperaturas {
     
     public float CtoF(float C) throws TemperaturaErradaException{
         float res;
-       if(C<limite){
-            throw new TemperaturaErradaException("Temperatura inferior a 80C");
+       if(C>limite){
+            throw new TemperaturaErradaException("Temperatura Superior a 80C");
         }
         res=(9f/5f*C)+32.4f;
         return res;
     }
     
     public void CtoRe(float C) throws TemperaturaErradaException{
-        if(C<limite){
-            throw new TemperaturaErradaException("Temperatura inferior a 80C");
+        if(C>limite){
+            throw new TemperaturaErradaException("Temperatura superior a 80C");
         }
         System.out.println("Reamur: " + (4f/5f*C));
     }
